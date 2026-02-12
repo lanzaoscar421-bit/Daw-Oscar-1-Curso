20fcd1d4dab3c8cde268ac4582acd90461256da0
COMPU-S033\daw1 <lanzaoscar421@gmail.com>
2026-02-12 09:13:38 +0100

    no message

---
+++ Ficheros-02-Oscar/src/Main.java
@@ -0,0 +1,128 @@
+import java.io.*;
+import java.util.InputMismatchException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+
+        Scanner sc = new Scanner(System.in);
+
+
+        final String path = ".\\src\\resources\\";
+        final String fileName = "Almacen.dat";
+
+        LinkedList<Producto> listaProductos = new LinkedList<>();
+
+
+
+        String opcion = "";
+
+        do {
+
+            Menu();
+
+
+            opcion = sc.nextLine();
+
+
+            switch (opcion) {
+                case "1":
+
+                    try {
+                        System.out.println("Inserte el codigo");
+                        String codigoInsert = sc.nextLine();
+
+                        System.out.println("Inserte el nombre");
+                        String nombreInsert = sc.nextLine();
+
+                        System.out.println("Inserte La cantidad");
+                        int cantidadInsert = sc.nextInt();
+
+                        System.out.println("Inserte el precio");
+                        double precioInsert = sc.nextDouble();
+                        sc.nextLine();
+
+
+                        listaProductos.add(new Producto(codigoInsert, nombreInsert, cantidadInsert, precioInsert));
+
+                    }catch (InputMismatchException e){
+                        System.out.println("Error, porfavor inserte bien los datos");
+                    }
+
+                    System.out.println("Producto añadido");
+
+                    break;
+                case "2":
+
+                    if (listaProductos.isEmpty()) {
+                        System.out.println("No hay productos.");
+                    } else {
+                        for (Producto p : listaProductos) {
+                            System.out.println(p);
+                        }
+                    }
+                    
+                    break;
+                case "3":
+
+                    System.out.println("Inserte el codigo para eliminar el producto" );
+                    String codigoEliminar = sc.nextLine();
+
+
+                    boolean eliminado = listaProductos.remove(codigoEliminar);
+
+                    if (eliminado) {
+                        System.out.println("Producto eliminado");
+                    }else{
+                        System.out.println("No existe el producto para eliminar el producto");
+                    }
+
+                    break;
+                case "4":
+
+                    try(FileOutputStream file = new FileOutputStream(path + fileName,true);
+                    DataOutputStream writer = new DataOutputStream(file)){
+
+                        for (Producto producto : listaProductos) {
+                            writer.writeUTF(producto.getCodigo());
+                            writer.writeUTF(producto.getNombre());
+                            writer.writeInt(producto.getCantidad());
+                            writer.writeDouble(producto.getPrecio());
+                        }
+
+                    }catch (IOException e) {
+                        System.out.println("Error al escribir el archivo" + e.getMessage());
+                    }
+                    break;
+                case "5":
+                    System.out.println("Adios.");
+                    break;
+
+                default:
+                    System.out.println("Porfavor, inserte un numero entre el 1-5");
+                    break;
+
+            }
+
+
+        }while(!opcion.equals("5"));
+
+
+
+        //Metodos
+    }
+
+
+
+    private static void Menu() {
+        System.out.println("1. Crear producto");
+        System.out.println("2. Mostrar productos existentes");
+        System.out.println("3. Eliminar producto por código");
+        System.out.println("4. Guardar producto en el fichero");
+        System.out.println("5. Salir");
+    }
+}
---
+++ Ficheros-02-Oscar/src/Producto.java
@@ -0,0 +1,59 @@
+public class Producto {
+
+    private String codigo;
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto(String codigo, String nombre, int cantidad, double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    //
+
+    public String toFileString() {
+        return codigo + ";" + nombre + ";" + cantidad + ";" + precio;
+    }
+
+    //Metodos
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "codigo='" + codigo + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", cantidad=" + cantidad +
+                ", precio=" + precio +
+                '}';
+    }
+}
