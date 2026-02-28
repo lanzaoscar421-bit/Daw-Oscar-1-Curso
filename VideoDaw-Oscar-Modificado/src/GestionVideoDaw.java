77b5f96e9a05c927f8788626c58813e91808a217
DESKTOP-KS7Q6HV\lanza <lanzaoscar421@gmail.com>
2026-02-28 14:24:31 +0100

    no message

--- VideoDaw-Oscar-Modificado/src/GestionVideoDaw.java
+++ VideoDaw-Oscar-Modificado/src/GestionVideoDaw.java
@@ -14,7 +14,7 @@ import Excepciones.*;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class GestionVideoDaw {
-    public static <buffer> void main(String[] args) {
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
