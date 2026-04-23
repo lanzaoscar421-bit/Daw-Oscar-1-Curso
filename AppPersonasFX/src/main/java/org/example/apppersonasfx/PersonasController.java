64f52d7bc2f09ca2954876efacd8ef2935c503d0
COMPU-S033\daw1 <lanzaoscar421@gmail.com>
2026-04-23 09:50:00 +0200

    no message

--- AppPersonasFX/src/main/java/org/example/apppersonasfx/PersonasController.java
+++ AppPersonasFX/src/main/java/org/example/apppersonasfx/PersonasController.java
@@ -1,9 +1,17 @@
 package org.example.apppersonasfx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import org.example.apppersonasfx.model.Persona;
 
 public class PersonasController {
+
+    Persona pp;
+    private ObservableList<Persona> personas = FXCollections.observableArrayList();
+
+
     @FXML
     private Label welcomeText;
 
